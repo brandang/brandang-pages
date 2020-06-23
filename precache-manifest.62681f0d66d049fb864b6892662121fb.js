7d1882e99c89e80a7d55c8fe223ed6b7385f2100
Brandon <brandon.dang1234@gmail.com>
2020-06-22 23:50:05 -0400

    Updates

---
+++ precache-manifest.62681f0d66d049fb864b6892662121fb.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fbb86a56a38d518eed5ebbca7e1f95d",
+    "url": "/brandang-pages/index.html"
+  },
+  {
+    "revision": "8484057891a07fb1f7fc",
+    "url": "/brandang-pages/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "dfa16d3fa829e772433d",
+    "url": "/brandang-pages/static/js/2.ea012151.chunk.js"
+  },
+  {
+    "revision": "f8cce587217730bf2bca2011dec1a03b",
+    "url": "/brandang-pages/static/js/2.ea012151.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8484057891a07fb1f7fc",
+    "url": "/brandang-pages/static/js/main.44309e30.chunk.js"
+  },
+  {
+    "revision": "6b7ac51e26a5a301f60b",
+    "url": "/brandang-pages/static/js/runtime-main.3980553d.js"
+  },
+  {
+    "revision": "6fdbcf9dc963d5589716673f7efdada5",
+    "url": "/brandang-pages/static/media/Resume.6fdbcf9d.pdf"
+  },
+  {
+    "revision": "567c837a99a25999e87620aa4afcc44a",
+    "url": "/brandang-pages/static/media/aboutMe.567c837a.JPG"
+  },
+  {
+    "revision": "5b357908babe6bae076c260e4100c180",
+    "url": "/brandang-pages/static/media/backgroundImage.5b357908.jpg"
+  },
+  {
+    "revision": "27cd9d820d68be7a5f3fb873d432f7b1",
+    "url": "/brandang-pages/static/media/blackBackground.27cd9d82.jpg"
+  }
+]);
