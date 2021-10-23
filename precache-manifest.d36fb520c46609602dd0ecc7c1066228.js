8b174c9865817b913ceafd81785adb5e2736d870
Brandon <brandon.dang1234@gmail.com>
2021-10-23 00:00:01 -0400

    Updates

---
+++ precache-manifest.d36fb520c46609602dd0ecc7c1066228.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69d3f84d3ca895192fb86978e109ae00",
+    "url": "/brandang-pages/index.html"
+  },
+  {
+    "revision": "7e7002e9ff0cc5692ba5",
+    "url": "/brandang-pages/static/css/2.d48510fc.chunk.css"
+  },
+  {
+    "revision": "8de7da8b3cf6e5846798",
+    "url": "/brandang-pages/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "7e7002e9ff0cc5692ba5",
+    "url": "/brandang-pages/static/js/2.7bfe160f.chunk.js"
+  },
+  {
+    "revision": "142b97d2dea2af1ed62d7bec825edd7a",
+    "url": "/brandang-pages/static/js/2.7bfe160f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8de7da8b3cf6e5846798",
+    "url": "/brandang-pages/static/js/main.210a67e8.chunk.js"
+  },
+  {
+    "revision": "68c4e988073154663735",
+    "url": "/brandang-pages/static/js/runtime-main.68262221.js"
+  },
+  {
+    "revision": "ea5c6174eb0fbac5d2ac53294d3fdabf",
+    "url": "/brandang-pages/static/media/Resume.ea5c6174.pdf"
+  },
+  {
+    "revision": "a88798366a77fdf0ebf1e0ff0eeb3fff",
+    "url": "/brandang-pages/static/media/backgroundImage.a8879836.jpg"
+  }
+]);
